ed187f1ca6a46dc39d8a9ed589db10b9dc3a41f4
Curiouscat <adwaitnaik2@gmail.com>
2020-02-22 09:36:47 +0530

    Quadtree implementation for random sampling

---
+++ Quadtree/quadtree.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+from quadtree1 import Rectangle, Quadtree, Point
+import random
+from matplotlib import pyplot
+
+
+l = 10
+rect = Rectangle(l/2,l/2,l/2)
+qtree = Quadtree(rect)
+
+points = list()
+for i in range(100):
+    p = Point(random.uniform(0, l), random.uniform(0, l))
+    points.append(p)
+    qtree.Insert(p)
+
+area = Rectangle(random.uniform(0, l), random.uniform(0, l), random.uniform(3, 4))
+points_in_area = qtree.Query(area)
+
+pyplot.figure()
+for p in points:
+    p.show()
+for p in points_in_area:
+    p.show("red")
+qtree.show()
+area.show("red")
+pyplot.xlim(-0.5, l + 0.5)
+pyplot.ylim(-0.5, l + 0.5)
+pyplot.gca().set_aspect('equal')
+pyplot.savefig("img.png")
+pyplot.show()
+pyplot.close()
+
+
+# In[ ]:
+
+
+
+
